aa72db4f100f5836c050a927e83772c0fae282ab
anusornkun <anusorn.ku98@gmail.com>
2020-05-14 14:24:18 +0700

    push all

---
+++ botpush.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+require "vendor/autoload.php";
+
+$access_token = '02dKuXg/pAqJaw5qw1iTgQ+KW35A469PaU6ICHUVdASbiKHIFFB1kLnz64FtpVKFMowKGr9KH3ydzI94KvbYlhdm+P+Bt6gSTuqWUNPGu0g2LSHKuc09VcF8oiFDrz24J5klyQoSGsOR/fbidRa1UgdB04t89/1O/w1cDnyilFU=';
+
+$channelSecret = 'a5b0bf20a231cb52c7d23bf2034b7a23';
+
+$pushID = 'U25cc1c3fc1dd7462abb26f845ab76d7d';
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello worldเด้อๆๆๆ');
+$response = $bot->pushMessage($pushID, $textMessageBuilder);
+
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+
+
+
+
+
+
+
