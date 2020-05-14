dde127498504c6f2314eecf7d26be66ec1eb8733
anusornkun <anusorn.ku98@gmail.com>
2020-05-14 17:16:37 +0700

    revise json 3

--- sentbotpush-json.php
+++ sentbotpush-json.php
@@ -5,11 +5,7 @@ function sent_line_pushjson($message, $tokenkey)
 {
 $ch = curl_init();
 curl_setopt( $ch, CURLOPT_URL, "https://api.line.me/v2/bot/message/push");
-curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, 0);
-curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, 0);
-curl_setopt( $ch, CURLOPT_POST, 1);
 curl_setopt( $ch, CURLOPT_POSTFIELDS, "message=$message");
-curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
 $headers = array( "Content-type: application/json", "Authorization: Bearer $tokenkey", );
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
