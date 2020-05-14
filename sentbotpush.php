a1742012590b83fbab573e23440347f71293408d
anusornkun <anusorn.ku98@gmail.com>
2020-05-14 15:38:48 +0700

    revise3

--- sentbotpush.php
+++ sentbotpush.php
@@ -1,10 +1,10 @@
 <?php
-function sent_line_push($message, $token)
 
+$mesg = $_POST['mesg'];
+$message =  $mesg;
 
 require "vendor/autoload.php";
 
-$mesg = $_POST['mesg'];
 
 $access_token = '02dKuXg/pAqJaw5qw1iTgQ+KW35A469PaU6ICHUVdASbiKHIFFB1kLnz64FtpVKFMowKGr9KH3ydzI94KvbYlhdm+P+Bt6gSTuqWUNPGu0g2LSHKuc09VcF8oiFDrz24J5klyQoSGsOR/fbidRa1UgdB04t89/1O/w1cDnyilFU=';
 
@@ -15,7 +15,7 @@ $pushID = 'U25cc1c3fc1dd7462abb26f845ab76d7d';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('$mesg');
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message);
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
