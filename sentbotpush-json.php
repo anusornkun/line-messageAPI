2c4ca4e697f8c788e882e7a099730f0ca9045247
anusornkun <anusorn.ku98@gmail.com>
2020-05-14 17:25:13 +0700

    revise json 5

--- sentbotpush-json.php
+++ sentbotpush-json.php
@@ -3,22 +3,19 @@
 
 function sent_line_pushjson($message, $tokenkey)
 {
-    curl -X POST \
-    https://api.line.me/v2/bot/message/push \
-    -H 'Authorization: Bearer 02dKuXg/pAqJaw5qw1iTgQ+KW35A469PaU6ICHUVdASbiKHIFFB1kLnz64FtpVKFMowKGr9KH3ydzI94KvbYlhdm+P+Bt6gSTuqWUNPGu0g2LSHKuc09VcF8oiFDrz24J5klyQoSGsOR/fbidRa1UgdB04t89/1O/w1cDnyilFU=' \
-    -H 'Content-Type: application/json' \
-    -d '{
-      "messages": [
-          {
-              "type": "text",
-              "text": "Hello, world1"
-          },
-          {
-              "type": "text",
-              "text": "Hello, world2"
-          }
-      ]
-  }'
+CURL *hnd = curl_easy_init();
+
+curl_easy_setopt(hnd, CURLOPT_CUSTOMREQUEST, "POST");
+curl_easy_setopt(hnd, CURLOPT_URL, "https://api.line.me/v2/bot/message/push");
+
+struct curl_slist *headers = NULL;
+headers = curl_slist_append(headers, "Content-Type: application/json");
+headers = curl_slist_append(headers, "Authorization: Bearer 02dKuXg/pAqJaw5qw1iTgQ+KW35A469PaU6ICHUVdASbiKHIFFB1kLnz64FtpVKFMowKGr9KH3ydzI94KvbYlhdm+P+Bt6gSTuqWUNPGu0g2LSHKuc09VcF8oiFDrz24J5klyQoSGsOR/fbidRa1UgdB04t89/1O/w1cDnyilFU=");
+curl_easy_setopt(hnd, CURLOPT_HTTPHEADER, headers);
+
+curl_easy_setopt(hnd, CURLOPT_POSTFIELDS, "{\n    \"messages\": [\n        {\n            \"type\": \"text\",\n            \"text\": \"Hello, world1\"\n        },\n        {\n            \"type\": \"text\",\n            \"text\": \"Hello, world2\"\n        }\n    ]\n}");
+
+CURLcode ret = curl_easy_perform(hnd);
 }
 $tokenkey = $_POST['tokenkey'];
 // $secretkey = $_POST['secretkey'];
