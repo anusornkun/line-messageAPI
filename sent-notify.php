c132b082d1c5e5d5e8282c70fd520c94947a4b75
anusornkun <anusorn.ku98@gmail.com>
2020-05-28 12:20:09 +0700

    test R5

--- sent-notify.php
+++ sent-notify.php
@@ -1,14 +1,16 @@
 <?php
 function sent_line_notify($message, $token)
 {
+    $headers = array( "Content-type: application/x-www-form-urlencoded", "Authorization: Bearer $token", "System-Type: PK" );
 $ch = curl_init();
+
 curl_setopt( $ch, CURLOPT_URL, "https://notify-api.line.me/api/notify");
 curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, 0);
 curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, 0);
 curl_setopt( $ch, CURLOPT_POST, 1);
-curl_setopt( $ch, CURLOPT_POSTFIELDS, "message=$message + $headers");
+curl_setopt( $ch, CURLOPT_POSTFIELDS, "message=$headers : $message");
 curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
-$headers = array( "Content-type: application/x-www-form-urlencoded", "Authorization: Bearer $token", "System-Type: PK" );
+
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
 $result = curl_exec( $ch );
