85159d181c04641bc46951ce854c297153d103d2
anusornkun <anusorn.ku98@gmail.com>
2020-05-28 12:08:45 +0700

    test header request

--- sent-notify.php
+++ sent-notify.php
@@ -6,7 +6,7 @@ curl_setopt( $ch, CURLOPT_URL, "https://notify-api.line.me/api/notify");
 curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, 0);
 curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, 0);
 curl_setopt( $ch, CURLOPT_POST, 1);
-curl_setopt( $ch, CURLOPT_POSTFIELDS, "message=$message");
+curl_setopt( $ch, CURLOPT_POSTFIELDS, "message=$message + $headers");
 curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
 $headers = array( "Content-type: application/x-www-form-urlencoded", "Authorization: Bearer $token", "System-Type: PK" );
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
