c867257a994411820ec500ca6560335d00a7a3b7
anusornkun <anusorn.ku98@gmail.com>
2020-05-28 12:01:43 +0700

    add Header (Test)

--- sent-notify.php
+++ sent-notify.php
@@ -8,7 +8,7 @@ curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, 0);
 curl_setopt( $ch, CURLOPT_POST, 1);
 curl_setopt( $ch, CURLOPT_POSTFIELDS, "message=$message");
 curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
-$headers = array( "Content-type: application/x-www-form-urlencoded", "Authorization: Bearer $token", );
+$headers = array( "Content-type: application/x-www-form-urlencoded", "Authorization: Bearer $token", "System-Type: PK" );
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
 $result = curl_exec( $ch );
